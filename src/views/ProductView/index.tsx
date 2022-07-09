8d76a237800ed5a69e58eaefa02bf35e9eca0e75
EnriqueEA <enrique19enrique@gmail.com>
2022-07-08 20:40:53 -0500

    commented lines

--- src/views/ProductView/index.tsx
+++ src/views/ProductView/index.tsx
@@ -1,10 +1,10 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { getCollection } from "../../firebase/firestore";
 import { Product } from "../../interfaces";
 
 const ProductView = () => {
-   const [product, setProduct] = useState();
+   // const [product, setProduct] = useState();
    const { id } = useParams();
 
    useEffect(() => {
